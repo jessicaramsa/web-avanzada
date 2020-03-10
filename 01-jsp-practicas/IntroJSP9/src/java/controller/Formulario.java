1a0046f6cf1692dfa2a196d89b4c6cac2b604664
Jéssica Ramírez <jessicaramsa@gmail.com>
2020-03-10 12:11:54 -0600

    practica 9

--- 01-jsp-practicas/IntroJSP9/src/java/controller/Formulario.java
+++ 01-jsp-practicas/IntroJSP9/src/java/controller/Formulario.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package controller;
 
 import java.io.Serializable;
@@ -6,9 +11,9 @@ import javax.faces.bean.RequestScoped;
 
 /**
  *
- * @author Uroboros
+ * @author jessicaramsa
  */
-@ManagedBean(name="formulario")
+@ManagedBean
 @RequestScoped
 public class Formulario implements Serializable {
     Boolean recibirInfo;
